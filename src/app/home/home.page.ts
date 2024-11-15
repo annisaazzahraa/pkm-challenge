3dc293d8061cf9189997eb265bd17f8a0733190f
jibrail0398 <jibrailadji0398@gmail.com>
2024-11-15 18:02:18 +0700

    Template awal challenge

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,42 @@ export class HomePage {
 
   constructor() {}
 
+  classData = [
+    "IF22A",
+    "IF22B",
+    "IF22C",
+    "IF22D",
+    "IF22E",
+    "IF22F",
+    "IF22G"
+  ]
+
+  personData = [
+    {
+      name : "Jibrail",
+      class : "IF22A",
+      nim : "22416255201274"
+    },
+    {
+      name : "Bayu",
+      class : "IF22A",
+      nim : "22416255201284"
+    },
+    {
+      name : "Alfian",
+      class : "IF22A",
+      nim : "22416255201213"
+    }
+  ]
+
+
+  name ="";
+  class = "";
+  nim = "";
+
+
+  submitData(){
+
+  }
+
 }
